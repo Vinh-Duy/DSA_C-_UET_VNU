0b35216211315aa76bfffd20f2d89b6b5962f7c5
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 06:31:28 +0700

    mushroom classification

--- test.cpp
+++ test.cpp
@@ -6,8 +6,19 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    long long a,b,c;
-    cin >> a >> b >> c;
-    cout << a*b%c;
+    long long a;
+    cin >> a;
+    if (a>=9.0){
+        cout << "VERY TOXIC";
+    }
+    else if (a >= 5.0 && a < 9.0)
+    {
+        cout << "TOXIC";
+    }
+    else if (a<5.0)
+    {
+        cout << "SAFE";
+    }
+    
     return 0;
 }
