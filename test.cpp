c5476b8f2fef79390c984c2410d03197678e2ba4
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 06:24:53 +0700

    Complex multiplication

--- test.cpp
+++ test.cpp
@@ -6,8 +6,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int a,b;
-    cin >> a >> b;
-    cout << a/b;
+    long long a,b,c;
+    cin >> a >> b >> c;
+    cout << a*b%c;
     return 0;
 }
