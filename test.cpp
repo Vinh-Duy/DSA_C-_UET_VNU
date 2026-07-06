9ed14c2911483bf278405d71d01ec6f1227e3c43
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 06:42:55 +0700

    Integer equation

--- test.cpp
+++ test.cpp
@@ -6,19 +6,19 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    long long a;
-    cin >> a;
-    if (a>=9.0){
-        cout << "VERY TOXIC";
+    long long a,b;
+    cin >> a >> b;
+    if (a == 0) {
+        if (b == 0) {
+            cout << "INFINITE SOLUTIONS\n";
+        } else {
+            cout << "NO SOLUTION\n";
+        }
+    } else {
+        if (b % a == 0) {
+            cout << -b / a << "\n";
+        } else {
+            cout << "NO SOLUTION\n";
+        }
     }
-    else if (a >= 5.0 && a < 9.0)
-    {
-        cout << "TOXIC";
-    }
-    else if (a<5.0)
-    {
-        cout << "SAFE";
-    }
-    
-    return 0;
 }
