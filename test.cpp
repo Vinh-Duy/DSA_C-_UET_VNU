761aaf5d6d751e2470b26040859be29c4e62af64
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 06:19:53 +0700

    A+B

--- test.cpp
+++ test.cpp
@@ -6,6 +6,8 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    cout << "Hello Marisa!";
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
     return 0;
 }
