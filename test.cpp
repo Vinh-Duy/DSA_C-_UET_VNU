df4dd05ec2981120abbc49d9e6b4c3e9797cbd2a
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 06:17:26 +0700

    Ex 0

--- test.cpp
+++ test.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -7,17 +6,6 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
-    freopen("test.inp","r",stdin);
-    freopen("test.out","w",stdout);
-
-    int n;
-    cin >> n;
-    for(int i = 0;i<(1<<n);++i){
-        for (int j = n-1; j>=0;--j){
-            cout << ((i>>j)&1);
-        }
-        cout << endl;
-    }
-    return 0; 
+    cout << "Hello Marisa!";
+    return 0;
 }
