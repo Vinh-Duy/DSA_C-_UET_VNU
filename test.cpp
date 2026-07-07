53f47c7f25e5c69b5d868f0607b7df07aeaad5b3
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 23:43:34 +0700

    subset

--- test.cpp
+++ test.cpp
@@ -1,27 +1,45 @@
-#include <iostream>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfenv>
+#include <cfloat>
+#include <cinttypes>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
 #include <string>
+#include <iostream>
+#include <istream>
+#include <ostream>
 
 using namespace std;
 
-long long n,k;
+int n,k;
 
-long long a[25];
-
-bool Try(int i, long long sum){
-    if (i == n-1){
-        if (sum == k){
-            return true;
-        }
-        return false;
-    }
-    if (sum > k){
-        return false;
-    }
-    // Chon phan tu a[i+1]
-    // Try(i + 1,sum + a[i+1]);
-    // Try(i + 1,sum);
-    return Try(i + 1,sum + a[i+1]) | Try(i + 1,sum);
-}
+set<set<int> > a;
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -29,24 +47,25 @@ int main(){
     cout.tie(0);
     // input
     cin >> n >> k;
-    for (int i = 0; i < n;i++){
-        cin >> a[i];
+    // __builtin_popcount(n) la so bit 1 trong bieu dien nhi phan cua n
+    
+    for (int i = 0; i < (1<<n);i++){
+        if (__builtin_popcount(i) == k){
+            set<int> b;
+            for (int j =0; j < n;j++){
+                if ((i>>j)&1){
+                    b.insert(j+1);
+                }
+            }
+            a.insert(b);
+        }
     }
 
-    // for (int i =0; i < n;i++){
-    //     cout << a[i] << endl;
-    // }
-
-    // Try(1,0);
-    // Try(1,a[1]);
-
-    bool result = Try(0,0) | Try(0,a[0]);
-
-    if (result == true){
-        cout << "YES";
-    }
-    else{
-        cout << "NO";
+    for (set<set<int> >::iterator c=a.begin();c!=a.end();c++){
+        for (set<int>::iterator x=(*c).begin();x!=(*c).end();x++){
+            cout << *x << " ";
+        }
+        cout << endl;
     }
 
     return 0;
