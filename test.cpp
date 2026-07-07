91f667ab3b1bc3813d98e76d1a5919d24e8994b4
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 22:17:33 +0700

    ABC string

--- test.cpp
+++ test.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+int n;
+
+void Try(int i,string s){
+    if (i==n){
+        cout << s << endl;
+        return;
+    }
+    if (s.back() == 'A'){
+        Try(i + 1,s + 'B');
+        Try(i + 1,s + 'C');
+    }
+    else if (s.back() == 'B'){
+        Try(i + 1,s + 'A');
+        Try(i + 1,s + 'C');
+    }
+    else {
+        Try(i + 1,s + 'A');
+        Try(i + 1,s + 'B');
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    long long a,b,c;
-    cin >> a>> b>> c;
-    if (a + b > c && b + c > a && a + c > b){
-        cout << "YES\n";
-    } else {
-        cout << "NO\n";
-    }
+
+    cin >> n;
+
+    Try(1,"A");
+    Try(1,"B");
+    Try(1,"C");
+
     return 0;
 }
