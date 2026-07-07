ac6a79f2f088e184cc695040484e92bb760c8bb8
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-07 11:16:21 +0700

    Age

--- test.cpp
+++ test.cpp
@@ -6,19 +6,19 @@ int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    long long a,b;
-    cin >> a >> b;
-    if (a == 0) {
-        if (b == 0) {
-            cout << "INFINITE SOLUTIONS\n";
-        } else {
-            cout << "NO SOLUTION\n";
+    int a,b,c,x,y,z;
+    cin >> a >> b >> c >> x >> y >> z;
+    if (c < z) {
+            cout << "1\n";
+        } 
+        else if (c == z && b < y) {
+            cout << "1\n";
+        } 
+        else if (c == z && b == y && a < x) {
+            cout << "1\n";
         }
-    } else {
-        if (b % a == 0) {
-            cout << -b / a << "\n";
-        } else {
-            cout << "NO SOLUTION\n";
+        else {
+            cout << "2\n";
         }
-    }
+    return 0;
 }
