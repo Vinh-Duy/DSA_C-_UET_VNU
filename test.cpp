c6c60a2f21526b5b2a0e7acec1c5b230a1a3b22f
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-08 22:21:26 +0700

    Group division

--- test.cpp
+++ test.cpp
@@ -1,59 +1,85 @@
-#include <cassert>
-#include <cctype>
-#include <cerrno>
-#include <cfenv>
-#include <cfloat>
-#include <cinttypes>
-#include <climits>
-#include <clocale>
-#include <cmath>
-#include <csetjmp>
-#include <csignal>
-#include <cstdarg>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <ctime>
-#include <cwchar>
-#include <cwctype>
-#include <array>
-#include <deque>
-#include <forward_list>
-#include <list>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <iostream>
-#include <istream>
-#include <iomanip>
-#include <ostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-double a;
+int n,k;
+vector<int> A;
+int target;
+int dp[1<<10];
+int parent[1<<10];
+int group[10];
+
+bool solve() {
+    int total = (1<<n) -1;
+    fill(dp, dp + (1<<n) ,-1);
+    dp[0] = 0;
+    for (int i =0 ;i < (1<<n); ++i){
+        if (dp[i] == -1) continue;
+        for (int j = 0; j<n;j++){
+            if(!(i & (1 << j))){
+                int next = i | (1<<j);
+                if (dp[i] + A[j] <= target){
+                    int sumn = (dp[i] + A[j] == target) ? 0 : dp[i] + A[j];
+                    if (dp[next] == -1){
+                        dp[next] = sumn;
+                        parent[next] = i;
+                    }
+                }
+            }
+        }
+    }
+    return dp[total] ==0;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     // input
-    cin >> a ;
+    cin >> n >> k ;
 
-    double c = a*2*3.14;
+    A.resize(n);
+    int sum = 0;
+    for (int i =0; i<n; i++){
+        cin >> A[i];
+        sum += A[i];
+    }
 
-    double s = a*a*3.14;
+    if (sum % k != 0){
+        cout << "ze\n";
+        return 0;
+    }
+
+    target = sum/k;
+
+    if (!solve()){
+        cout << "ze\n";
+        return 0;
+    }
+
+    int curmask = (1<<n) -1;
+    int curgroup = k;
+    int cursum = 0;
+
+    while (curmask > 0){
+        int premask = parent[curmask];
+        int diff = curmask ^ premask;
+        int idx = __builtin_ctz(diff);
+
+        group[idx] = curgroup;
+        cursum += A[idx];
+
+        if (cursum ==target){
+            curgroup --;
+            cursum = 0;
+        }
+        curmask = premask;
+    }
+
+    for (int i =0; i<n;++i){
+        cout << group[i] << (i ==n-1 ? "" :" ");
+    }
+    cout << "\n";
     
-    cout << fixed << setprecision(3) << c << " ";
-
-    cout << fixed << setprecision(3) << s;
-
     return 0;
 }
