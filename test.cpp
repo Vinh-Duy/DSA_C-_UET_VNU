a6c128f2a4a4872c37f2c0731daab566c7126b3c
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-09 20:11:41 +0700

    Fix knight's tour

--- test.cpp
+++ test.cpp
@@ -2,39 +2,39 @@
 
 using namespace std;
 
-const int N = 10;
+const int N = 15;
 
-int n, m, res[N][N], curStep = 1;
+int n, m, res[N][N];
 bool visited[N][N];
+bool found = false;
+
 int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
 int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
 
-// hàm kiểm tra ô (x, y) có nằm trong bảng chữ nhật nxm không
 bool inside(int x, int y) {
     return x >= 1 && x <= n && y >= 1 && y <= m;
 }
 
-bool success() {
-    for (int i = 1; i <= n; i++) for (int j = 1; j <= m; j++) {
-        if (!visited[i][j]) return false;
-    }
-    return true;
-}
-
-void backtrack(int x, int y) {
+void backtrack(int x, int y, int curStep) {
     visited[x][y] = true;
     res[x][y] = curStep;
-    curStep++;
-    if (success()) return;
+
+    if (curStep == n * m) {
+        found = true;
+        return;
+    }
+
     for (int k = 0; k <= 7; k++) {
         int x1 = x + dx[k];
         int y1 = y + dy[k];
+        
         if (inside(x1, y1) && !visited[x1][y1]) {
-            backtrack(x1, y1);
-            if (success()) return;
+            backtrack(x1, y1, curStep + 1);
+            
+            if (found) return;
+            
             visited[x1][y1] = false;
             res[x1][y1] = 0;
-            curStep--;
         }
     }
 }
@@ -44,14 +44,16 @@ signed main() {
     cin.tie(0);
     cout.tie(0);
 
-    freopen("test.inp", "r", stdin);
-    freopen("test.out", "w", stdout);
-
-    cin >> n >> m;
-
-    backtrack(1, 1);
-    
-    for (int i = 1; i <= n; i++) for (int j = 1; j <= m; j++) cout << res[i][j] << " \n"[j == m];
+    if (cin >> n >> m) {
+        backtrack(1, 1, 1);
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                cout << res[i][j] << (j == m ? "" : " ");
+            }
+            cout << "\n";
+        }
+    }
 
     return 0;
 }
