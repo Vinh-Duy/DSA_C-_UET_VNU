67f144e918c29d2b6932c5be12b56103df6a942b
Vinh-Duy <duy.buivinh2007@gmail.com>
2026-07-09 15:25:48 +0700

    Knight's tour

--- test.cpp
+++ test.cpp
@@ -2,59 +2,56 @@
 
 using namespace std;
 
-int n,k,target;
-vector<int> A;
-vector<int> group;
-vector<int> ans;
+const int N = 10;
 
-bool backtrack(int idx){
-    if (idx == n){
-        return true;
-    }
+int n, m, res[N][N], curStep = 1;
+bool visited[N][N];
+int dx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int dy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
 
-    for (int j = 0; j<k; j++){
-        if(group[j] + A[idx] <=target){
-            group[j] += A[idx];
-            ans[idx] = j+1;
-            if (backtrack(idx+1)) return true;
-            group[j] -= A[idx];
-        }
-    if (group[j]==0) break;
-    }
-    return false;
+// hàm kiểm tra ô (x, y) có nằm trong bảng chữ nhật nxm không
+bool inside(int x, int y) {
+    return x >= 1 && x <= n && y >= 1 && y <= m;
 }
 
-int main(){
+bool success() {
+    for (int i = 1; i <= n; i++) for (int j = 1; j <= m; j++) {
+        if (!visited[i][j]) return false;
+    }
+    return true;
+}
+
+void backtrack(int x, int y) {
+    visited[x][y] = true;
+    res[x][y] = curStep;
+    curStep++;
+    if (success()) return;
+    for (int k = 0; k <= 7; k++) {
+        int x1 = x + dx[k];
+        int y1 = y + dy[k];
+        if (inside(x1, y1) && !visited[x1][y1]) {
+            backtrack(x1, y1);
+            if (success()) return;
+            visited[x1][y1] = false;
+            res[x1][y1] = 0;
+            curStep--;
+        }
+    }
+}
+
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    // input
-    cin >> n >> k ;
-    A.resize(n);
-    ans.resize(n);
-    group.assign(k,0);
 
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> A[i];
-        sum += A[i];
-    }
+    freopen("test.inp", "r", stdin);
+    freopen("test.out", "w", stdout);
 
-    if (sum % k != 0) {
-        cout << "ze\n";
-        return 0;
-    }
+    cin >> n >> m;
 
-    target = sum / k;
-
-    if (backtrack(0)) {
-        for (int i = 0; i < n; i++) {
-            cout << ans[i] << (i == n - 1 ? "" : " ");
-        }
-        cout << "\n";
-    } else {
-        cout << "ze\n";
-    }
+    backtrack(1, 1);
+    
+    for (int i = 1; i <= n; i++) for (int j = 1; j <= m; j++) cout << res[i][j] << " \n"[j == m];
 
     return 0;
 }
